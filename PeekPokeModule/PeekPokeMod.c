274b40c267b6dc1e7e8b691e1d4510f28cfb782c
student <konghon@live.nl>
2018-02-28 13:13:27 +0100

    Code Compiles Able to communicate with sys file

--- PeekPokeModule/PeekPokeMod.c
+++ PeekPokeModule/PeekPokeMod.c
@@ -19,21 +19,41 @@ sysfs_show(	struct device *dev,
 			struct device_attribute *attr,
 			char *buffer)
 			{
-				printk(KERNINFO "sysfile_read(/sys/kernel/%s/%s) called", sysfs_dir, sysfs_file);
-				return sprintf(buffer, "/s", sysfs_buffer);
+				printk(KERN_INFO "sysfile_read(/sys/kernel/%s/%s) called", sysfs_dir, sysfs_file);
+				return sprintf(buffer, "%s", sysfs_buffer);
 			}
 			
+static ssize_t
+sysfs_store(struct device *dev,
+			struct device_attribute *attr,
+			const char *buffer,
+			size_t count)
+			{
+				used_buffer_size = count > sysfs_max_data_size ? sysfs_max_data_size :count;
+				printk(KERN_INFO "sysfile_write (/sys/kernel/%s/%s) called",sysfs_dir,sysfs_file);
+				memcpy(sysfs_buffer,buffer,used_buffer_size);
+				sysfs_buffer[used_buffer_size] = '\0';
+				return used_buffer_size;
+			}
+
 static DEVICE_ATTR(data, S_IWUGO | S_IRUGO, sysfs_show, sysfs_store);
 
 static struct attribute *attrs[]={
-	.&dev_attr_data.attr,
+	&dev_attr_data.attr,
 	NULL
 };
+
+static struct attribute_group attr_group = {
+	.attrs = attrs,
+};
+
 static struct kobject *peekpoke_obj = NULL;
 
 int __init sysfs_init(void)
 {
-	peekpoke_obj = kboject_create_and_add(sysfs_dir, kernel_kobj);
+	int result = 0;
+	
+	peekpoke_obj = kobject_create_and_add(sysfs_dir, kernel_kobj);
 	if(peekpoke_obj == NULL)
 	{
 		printk (KERN_INFO "%s module failed to load: create/add failed \n", sysfs_file);
@@ -43,12 +63,18 @@ int __init sysfs_init(void)
 	result = sysfs_create_group(peekpoke_obj, &attr_group);
 	if(result != 0)
 	{
-		printk (KERN_INFO "%s module failed to load: sysfs_create_group failed with result %d\n", sysfs_file, result):
-		return _ENOMEM;
+		printk (KERN_INFO "%s module failed to load: sysfs_create_group failed with result %d\n", sysfs_file, result);
+		return -ENOMEM;
 	}
 	
-	printk (KERNINFO "/sys/kernel/%s/%s created\n", sysfs-dir, sysfs_file);
+	printk (KERN_INFO "/sys/kernel/%s/%s created\n", sysfs_dir, sysfs_file);
 	return result;
+}
+
+void __exit sysfs_exit(void)
+{
+	kobject_put(peekpoke_obj);
+	printk (KERN_INFO "/sys/kernel/%s/%s removed\n", sysfs_dir,sysfs_file);
 }
 
 module_init(sysfs_init)
