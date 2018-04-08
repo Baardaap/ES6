b3b9dbe85b652ea9943546acd58515db193808f6
student <konghon@live.nl>
2018-04-08 21:13:04 +0200

    added stuuf

--- looplicht-0.1/main.c
+++ looplicht-0.1/main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include <signal.h>
+
 #include "i2c_ext.h"
 #include "pca9532.h"
 
@@ -12,10 +14,21 @@
 #define PWM_MAX 255
 #define INPUT_POWER_MAX 100
 
+volatile sig_atomic_t stopFlag = 0;
+
+static void handler(int signum) {
+	stopFlag = signum;
+}
+
 int main(int argc, char* argv[])
 {
     int dev;
 
+    struct sigaction action;
+    memset(&action, 0, sizeof(struct sigaction));
+    action.sa_handler = handler;
+    sigaction(SIGTERM, &action, NULL);
+ 
     /* Open the i2c master device */
     if ((dev = open(DEVICE, O_RDWR)) < 0)
     {
@@ -52,41 +65,45 @@ int main(int argc, char* argv[])
         { PCA9532_REGISTER_LS3, PCA9532_LEDMODE_OFF },
     };
 
-    int input;
-    int delay = DEFAULT_DELAY;
-    int pwm = 255;
+    float input;
+    float delay = DEFAULT_DELAY;
+    float pwm = 255;
     printf("Enter the light delay in Hz:\n");
-    if(scanf("%d", &input))
+    if(scanf("%f", &input))
     {
         //convert Hz to Microseconds for the usleep;
         delay = (1/input)*MICROSECOND;
     }
-    printf("Set delay is: %iHz\n", input);
+    printf("Set delay is: %fHz\n", input);
 
     printf("Enter the light power in %%:\n");
-    if(scanf("%d", &input))
+    if(scanf("%f", &input))
     {
         pwm = (input)*(PWM_MAX/INPUT_POWER_MAX);
         i2c_smbus_write_byte_data(dev, PCA9532_LEDMODE_PWM0, pwm);
     }
-    printf("Set power is: %i%%\n", input);
+    printf("Set power is: %f%%\n", input);
 
 
-        
-    for (i = 0; i < 16; i++)
-    {
-        if (i2c_smbus_write_byte_data(dev, states[i][0], states[i][1]))
+    while(!stopFlag)
+    {   
+        for (i = 0; i < 16; i++)
         {
-            printf("Couldn't set LED 0x%02X on register 0x%02X\n", (int)states[i][1],
-                   (int)states[i][0]);
-        }
-        else
-        {
-            printf("Set LED 0x%02X on register 0x%02X\n", (int)states[i][1],
-                   (int)states[i][0]);
-        }
+            if (i2c_smbus_write_byte_data(dev, states[i][0], states[i][1]))
+            {
+                printf("Couldn't set LED 0x%02X on register 0x%02X\n", (int)states[i][1],
+                    (int)states[i][0]);
+            }
+            else
+            {
+                printf("Set LED 0x%02X on register 0x%02X\n", (int)states[i][1],
+                    (int)states[i][0]);
+            }
 
-        usleep(delay);
+            usleep(delay);
+        }  
+        if (stopFlag)
+	    break;
     }
 
     close(dev);
