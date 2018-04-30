dde669605761d81d60721e750387a2feb553756c
Baardaap <ian_seelen@hotmail.com>
2018-04-30 16:40:27 +0200

    Changes nog van voor de vakantie

--- GPIO/GPIO.c
+++ GPIO/GPIO.c
@@ -1,8 +1,16 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
 #include <mach/hardware.h>
 
-#define P2_MUX_SET  0x40028028
+#define P2_MUX_SET      0x40028028
+#define P2_INP_STATE    0x4002801C
+#define P2_DIR_SET      0x40028010
 
-static ssize_t sysfs_show(struct device *dev, struct device_attribute *attr, char *buffer)
+#define P2_EMC_D_ENABLE_MASK        0x00000008
+#define P2_INPUTDIR_JOYSTICK_MASK   0x0000001F
+
+/* static ssize_t sysfs_show(struct device *dev, struct device_attribute *attr, char *buffer)
 {
     return 0;
 }
@@ -17,7 +25,7 @@ static int device_open(struct inode *inode, struct file *file)
     return 0;
 }
 
-static int device_release(struct inode *inode, struct file *file) 
+static int device_release(struct inode *inode, struct file *file)
 {
     return 0;
 }
@@ -32,7 +40,6 @@ static ssize_t device_write(struct file *file, const char *buff, size_t length, 
     return 0;
 }
 
-
 struct file_operations Fops =
 {
     .read = device_read,
@@ -44,16 +51,22 @@ struct file_operations Fops =
 static DEVICE_ATTR(data, S_IWUGO | S_IRUGO, sysfs_show, sysfs_store);
 static struct attribute *attrs[] = { &dev_attr_data.attr, NULL};
 static struct attribute_group attr_group = {.attrs = attrs,};
-static struct kobject *hello_obj = NULL;
+static struct kobject *hello_obj = NULL; */
 
 int init_module (void)
 {
-    io_p2v(P2_MUX_SET) &= 0x04;
+    uint32_t *address = io_p2v(P2_MUX_SET);
+    *address |= P2_EMC_D_ENABLE_MASK;         //Connect pins 19 - 31 from the EMC_D to the GPIO port 2
+    
+    address = io_p2v(P2_DIR_SET);
+    *address |= P2_INPUTDIR_JOYSTICK_MASK;    //Set pins 2.0 - 2.4 on input mode
 }
 
 void cleanup_module(void)
 {
-
+    uint32_t joystickStates = io_p2v(P2_INP_STATE);
+    joystickStates &= P2_INPUTDIR_JOYSTICK_MASK;
+    printk(KERN_INFO "Joystick value: %x\n", joystickStates);
 }
 
 MODULE_LICENSE("Dual BSD/GPL");
