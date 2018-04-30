b85bf637648e31ff98e6c37e0854a1a5f7c682ea
Baardaap <ian_seelen@hotmail.com>
2018-04-30 17:30:59 +0200

    hal setup

--- GPIO/GPIO Nice/GPIOLPC.c
+++ GPIO/GPIO Nice/GPIOLPC.c
@@ -1,1 +1,22 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <mach/hardware.h>
 #include "LPC3250pins.h"
+
+#define P2_MUX_SET      0x40028028
+#define P2_INP_STATE    0x4002801C
+#define P2_DIR_SET      0x40028010
+
+#define P2_EMC_D_ENABLE_MASK        0x00000008
+#define P2_INPUTDIR_JOYSTICK_MASK   0x0000001F
+
+void setPin(string LPCpin)
+{
+    return;
+}
+
+uint32_t readPin(string LPCpin)
+{
+    return 0;
+}
